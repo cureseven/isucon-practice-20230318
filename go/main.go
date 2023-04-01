7d3fb438d504b18ab0830d64bb172c8e9cef647d
masami <msihima0304@gmail.com>
2023-04-01 17:49:49 +0900

    feat: 🎸 修正した

--- go/main.go
+++ go/main.go
@@ -1098,7 +1098,7 @@ func updateTrendCache() {
 			cacheMutex.Unlock()
 		}
 
-		time.Sleep(200 * time.Millisecond)
+		time.Sleep(500 * time.Millisecond)
 	}
 }
 
@@ -1262,7 +1262,7 @@ func processConditionQueue() {
 	worker := func() {
 		for {
 
-			time.Sleep(200 * time.Millisecond)
+			time.Sleep(100 * time.Millisecond)
 			now := time.Now()
 			// キューの内容をローカル変数にコピー
 			func() { // 無名関数を追加
